937c6e33107d71de3adb6076ce2e485e3fd9582b
James Schumann <jschumann@gmail.com>
2016-05-09 07:46:21 -0400

    add mailgun library and test connection

--- mailgun-dashboard.php
+++ mailgun-dashboard.php
@@ -11,3 +11,23 @@
 * GitHub Branch: master
 */
 
+require 'vendor/autoload.php';
+use Mailgun\Mailgun;
+
+$mg = new Mailgun(MAILGUN_APIKEY);
+$domain = MAILGUN_DOMAIN;
+
+# For testing our connection to the API:
+# Get the last 25 log entries, and dump
+# info to the browser.
+$result = $mg->get("$domain/log", array('limit' => 25, 'skip'  => 0));
+
+$httpResponseCode = $result->http_response_code;
+$httpResponseBody = $result->http_response_body;
+
+# Iterate through the results and echo the message IDs.
+$logItems = $result->http_response_body->items;
+foreach($logItems as $logItem){
+    echo $logItem->message_id . "\n";
+}
+
