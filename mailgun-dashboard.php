5707c309b42da382126055f44b411e665c8fd029
James Schumann <jschumann@gmail.com>
2016-05-09 07:31:08 -0400

    add empty plugin file

---
+++ mailgun-dashboard.php
@@ -0,0 +1,13 @@
+<?php
+/*
+* Plugin Name: Mailgun Dashboard
+* Plugin URI: https://github.com/wrdsb/wordpress-plugin-mailgun-dashboard
+* Description: Dashboard widgets and admin pages for Mailgun status and stats
+* Author: WRDSB
+* Author URI: https://github.com/wrdsb
+* Version: 0.0.1
+* License: GPLv2 or later
+* GitHub Plugin URI: wrdsb/wordpress-plugin-mailgun-dashboard
+* GitHub Branch: master
+*/
+
