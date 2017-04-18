cc4f83c7655bdf9702cf760e407794b5b98a389b
James Schumann <jschumann@gmail.com>
2017-04-18 09:11:58 -0400

    version bump

--- mailgun-dashboard.php
+++ mailgun-dashboard.php
@@ -5,7 +5,7 @@
 * Description: Dashboard widgets and admin pages for Mailgun status and stats
 * Author: WRDSB
 * Author URI: https://github.com/wrdsb
-* Version: 0.0.1
+* Version: 0.0.2
 * License: GPLv2 or later
 * GitHub Plugin URI: wrdsb/wordpress-plugin-mailgun-dashboard
 * GitHub Branch: master
